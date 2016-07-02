05d481efd8b88e155d35cfff84a125d24846ba8e
Craig Andrews <craig@lazyrobot.org.uk>
2016-07-02 16:31:12 +0100

    Finish off Queue

--- queue/Queue.go
+++ queue/Queue.go
@@ -1,6 +1,5 @@
 package queue
 
-type Queue []string
 type QueueError struct {
 	msg string
 }
@@ -9,33 +8,54 @@ func (e QueueError) Error() string {
 	return e.msg
 }
 
-func (q Queue) Add(u string) Queue {
-	if q.Contains(u) {
+// Item represents a person with a job in the queue
+type Item struct {
+	name string
+	reason string
+}
+
+// Queue represents a list of waiting items
+type Queue []Item
+
+// Add appends an item to the queue unless it already exists
+func (q Queue) Add(i Item) Queue {
+	if q.Contains(i) {
 		return q
 	}
-	q = append(q, u)
+	q = append(q, i)
 	return q
 }
 
-func (q Queue) Contains(u string) bool {
+// Contains returns true if the item exists in the queue
+func (q Queue) Contains(i Item) bool {
 	for _, n := range q {
-		if n == u {
+		if n == i {
 			return true
 		}
 	}
 	return false
 }
 
-func (q Queue) Active() string {
+// Active returns the first item in the queue or panics if the queue is empty
+func (q Queue) Active() Item {
 	if len(q) > 0 {
 		return q[0]
 	}
-	return ""
+	panic(QueueError{"Queue is empty"})
 }
 
-func (q Queue) Remove(u string) Queue {
+// Waiting returns all items in the queue in order except the Active item
+func (q Queue) Waiting() []Item {
+	if len(q) > 0 {
+		return q[1:]
+	}
+	return []Item(q)
+}
+
+// Remove removes an item from the queue
+func (q Queue) Remove(i Item) Queue {
 	for ix := range q {
-		if q[ix] == u {
+		if q[ix] == i {
 			if ix == 0 {
 				return Queue(q[1:])
 			} else if ix == len(q) - 1 {
@@ -47,9 +67,20 @@ func (q Queue) Remove(u string) Queue {
 	return q
 }
 
+// Yield swaps the Active item with the first Waiting item
 func (q Queue) Yield() Queue {
 	if len(q) > 1 {
 		q[0], q[1] = q[1], q[0]
 	}
 	return q
+}
+
+// Barge adds a new item to the second place in the queue, or moves an existing item to second place
+func (q Queue) Barge(i Item) Queue {
+	if len(q) > 1 && q.Active() != i {
+		w := q.Remove(i).Waiting()
+		q := Queue{q.Active(), i}
+		return Queue(append(q, w...))
+	}
+	return q.Add(i)
 }
--- queue/queue_test.go
+++ queue/queue_test.go
@@ -1,6 +1,14 @@
 package queue
 
-import "testing"
+import (
+	"testing"
+	"reflect"
+)
+
+var john = Item{"john", "done some coding"}
+var jimmy = Item{"jimmy", "fix some bugs"}
+var mick = Item{"mick", "refactoring"}
+var colin = Item{"colin", "adding bugs"}
 
 func TestCreateQueue(t *testing.T) {
 	q := Queue{}
@@ -8,14 +16,16 @@ func TestCreateQueue(t *testing.T) {
 		t.Errorf("Expected empty queue, but has length %d", len(q))
 	}
 
-	active := q.Active()
-	if active != "" {
-		t.Errorf("Expected no active user but was '%s'", active)
-	}
+	defer func() {
+		if recover() == nil {
+			t.Errorf("Expected no active user but one was found")
+		}
+	}()
+	q.Active()
 }
 
 func TestCreateQueueWithEntries(t *testing.T) {
-	q := Queue{"mick", "john"}
+	q := Queue{mick, john}
 	if len(q) != 2 {
 		t.Errorf("Expected 2 entries in queue, but found %d", len(q))
 	}
@@ -23,7 +33,7 @@ func TestCreateQueueWithEntries(t *testing.T) {
 
 func TestAddImmutable(t *testing.T) {
 	q := Queue{}
-	q.Add("mick")
+	q.Add(mick)
 	if len(q) != 0 {
 		t.Errorf("Expected empty queue, but has length %d", len(q))
 	}
@@ -31,86 +41,189 @@ func TestAddImmutable(t *testing.T) {
 
 func TestAdd(t *testing.T) {
 	q := Queue{}
-	q = q.Add("mick")
+	q = q.Add(mick)
 	if len(q) != 1 {
 		t.Errorf("Expected 1 entry in queue, but found %d", len(q))
 	}
 
 	active := q.Active()
-	if active != "mick" {
+	if active != mick {
 		t.Errorf("Expected active user to be 'mick' but was '%s'", active)
 	}
 }
 
 func TestAddDuplicate(t *testing.T) {
-	q := Queue{"mick"}
-	q = q.Add("mick")
+	q := Queue{mick}
+	q = q.Add(mick)
 	if len(q) != 1 {
 		t.Errorf("Expecte 1 entry in queue, but found %d", len(q))
 	}
 }
 
+func TestAddWithDifferentReason(t *testing.T) {
+	q := Queue{mick}
+	q = q.Add(Item{"mick", "wrote tests"})
+	if len(q) != 2 {
+		t.Errorf("Expecte 2 entry in queue, but found %d", len(q))
+	}
+}
+
+func TestWaitingWhenEmpty(t *testing.T) {
+	q := Queue{}
+	w := q.Waiting()
+	if len(w) > 0 {
+		t.Errorf("Expected 0 waiting, but found %d", len(w))
+	}
+}
+
+func TestWaitingWhenOnlyOne(t *testing.T) {
+	q := Queue{mick}
+	w := q.Waiting()
+	if len(w) > 0 {
+		t.Errorf("Expected 0 waiting, but found %d", len(w))
+	}
+}
+
+func TestWaitingWhenMoreThanOne(t *testing.T) {
+	q := Queue{mick, john, jimmy}
+	expected := []Item{john, jimmy}
+	if !reflect.DeepEqual(q.Waiting(), expected) {
+		t.Errorf("Expected %v waiting, but found %v", expected, q.Waiting())
+	}
+}
+
 func TestRemoveImmutable(t *testing.T) {
-	q := Queue{"mick"}
-	q.Remove("mick")
+	q := Queue{mick}
+	q.Remove(mick)
 	if len(q) != 1 {
 		t.Errorf("Expected 1 entry in queue, but found %d", len(q))
 	}
 }
 
 func TestRemoveActive(t *testing.T) {
-	q := Queue{"mick", "john"}
-	q = q.Remove("mick")
+	q := Queue{mick, john}
+	q = q.Remove(mick)
 	if len(q) != 1 {
 		t.Errorf("Expected 1 entry in queue, but found %d", len(q))
 	}
-	if q.Active() != "john" {
+
+	if q.Active() != john {
 		t.Errorf("Expected 'john' to be active but was '%s'", q.Active())
 	}
 }
 
 func TestRemoveMiddle(t *testing.T) {
-	q := Queue{"mick", "jimmy", "john"}
-	q = q.Remove("jimmy")
+	q := Queue{mick, jimmy, john}
+	q = q.Remove(jimmy)
 	if len(q) != 2 {
 		t.Errorf("Expected 2 entry in queue, but found %d", len(q))
 	}
-	if q.Contains("jimmy") {
+	if q.Contains(jimmy) {
 		t.Errorf("Expected 'jimmy' to be removed but was found")
 	}
 }
 
 func TestRemoveLast(t *testing.T) {
-	q := Queue{"mick", "john"}
-	q = q.Remove("john")
+	q := Queue{mick, john}
+	q = q.Remove(john)
 	if len(q) != 1 {
 		t.Errorf("Expected 1 entry in queue, but found %d", len(q))
 	}
-	if q.Contains("john") {
+	if q.Contains(john) {
 		t.Errorf("Expected 'john' to be removed but was found")
 	}
 }
 
 func TestRemoveNotPresent(t *testing.T) {
-	q := Queue{"mick", "john"}
-	q = q.Remove("jimmy")
+	q := Queue{mick, john}
+	q = q.Remove(jimmy)
 	if len(q) != 2 {
 		t.Errorf("Expected 2 entry in queue, but found %d", len(q))
 	}
 }
 
 func TestYield(t *testing.T) {
-	q := Queue{"mick", "john"}
+	q := Queue{mick, john}
 	q = q.Yield()
-	if q.Active() != "john" {
+	if q.Active() != john {
 		t.Errorf("Expected 'john' to be active but was '%s'", q.Active())
 	}
 }
 
 func TestYieldAlone(t *testing.T) {
-	q := Queue{"mick"}
+	q := Queue{mick}
 	q = q.Yield()
-	if q.Active() != "mick" {
-		t.Errorf("Expected 'john' to be active but was '%s'", q.Active())
+	if q.Active() != mick {
+		t.Errorf("Expected 'mick' to be active but was '%s'", q.Active())
+	}
+}
+
+func TestBargeWhenEmpty(t *testing.T) {
+	q := Queue{}
+	q = q.Barge(mick)
+	if q.Active() != mick {
+		t.Errorf("Expected 'mick' to be active but was '%s'", q.Active())
+	}
+}
+
+func TestBargeWhenActive(t *testing.T) {
+	q := Queue{mick, john}
+	q = q.Barge(mick)
+	if q.Active() != mick {
+		t.Errorf("Expected 'mick' to be active but was '%s'", q.Active())
+	}
+}
+
+func TestBargeWhenOnlyOne(t *testing.T) {
+	q := Queue{john}
+	q = q.Barge(mick)
+	expected := []Item{mick}
+	if !reflect.DeepEqual(q.Waiting(), expected) {
+		t.Errorf("Expected waiting to be %v but was %v", expected, q.Waiting())
+	}
+}
+
+func TestBargeWhenOnlyTwoAndAlreadySecond(t *testing.T) {
+	q := Queue{john, mick}
+	q = q.Barge(mick)
+	expected := []Item{mick}
+	if !reflect.DeepEqual(q.Waiting(), expected) {
+		t.Errorf("Expected waiting to be %v but was %v", expected, q.Waiting())
+	}
+}
+
+func TestBargeWhenOnlyTwo(t *testing.T) {
+	q := Queue{john, jimmy}
+	q = q.Barge(mick)
+	expected := []Item{mick, jimmy}
+	if !reflect.DeepEqual(q.Waiting(), expected) {
+		t.Errorf("Expected waiting to be %v but was %v", expected, q.Waiting())
+	}
+}
+
+func TestBargeWhenAlreadySecond(t *testing.T) {
+	q := Queue{john, mick}
+	q = q.Barge(mick)
+	expected := []Item{mick}
+	if !reflect.DeepEqual(q.Waiting(), expected) {
+		t.Errorf("Expected waiting to be %v but was %v", expected, q.Waiting())
+	}
+}
+
+func TestBargeWhenNotPresent(t *testing.T) {
+	q := Queue{john, colin, jimmy}
+	q = q.Barge(mick)
+	expected := []Item{mick, colin, jimmy}
+	if !reflect.DeepEqual(q.Waiting(), expected) {
+		t.Errorf("Expected waiting to be %v but was %v", expected, q.Waiting())
+	}
+}
+
+func TestBargeWhenPresent(t *testing.T) {
+	q := Queue{john, colin, mick, jimmy}
+	q = q.Barge(mick)
+	expected := []Item{mick, colin, jimmy}
+	if !reflect.DeepEqual(q.Waiting(), expected) {
+		t.Errorf("Expected waiting to be %v but was %v", expected, q.Waiting())
 	}
 }
