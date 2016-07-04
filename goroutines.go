451277856706c3fe2dcd10eee980483fc58f709e
Craig Andrews <craig@lazyrobot.org.uk>
2016-07-04 22:57:39 +0100

    Very tidy dispatch

---
+++ goroutines.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"log"
+	"reflect"
+	"strings"
+
+	"github.com/doozr/qbot/command"
+	"github.com/doozr/qbot/queue"
+	"github.com/doozr/qbot/slack"
+	"github.com/doozr/qbot/usercache"
+)
+
+func MessageDispatch(name string, q queue.Queue, commands command.Command,
+	messageChan chan slack.RtmMessage, saveChan chan queue.Queue, notifyChan chan Notification) {
+
+	splitUser := func(u string) (username string, reason string) {
+		args := strings.SplitN(u, " ", 2)
+		username = args[0]
+		reason = ""
+		if len(args) > 1 {
+			reason = args[1]
+		}
+		return
+	}
+
+	logNotification := func(n string) {
+		for _, l := range strings.Split(n, "\n") {
+			if l != "" {
+				log.Println(l)
+			}
+		}
+	}
+
+	for m := range messageChan {
+		parts := strings.SplitN(m.Text, " ", 3)
+
+		if len(parts) < 2 {
+			continue
+		}
+		cmd := parts[1]
+
+		rest := ""
+		if len(parts) > 2 {
+			rest = parts[2]
+		}
+
+		oq := q
+		n := ""
+
+		switch cmd {
+		case "join":
+			q, n = commands.Join(q, m.User, rest)
+		case "leave":
+			q, n = commands.Leave(q, m.User, rest)
+		case "done":
+			q, n = commands.Done(q, m.User)
+		case "yield":
+			q, n = commands.Yield(q, m.User)
+		case "barge":
+			q, n = commands.Barge(q, m.User, rest)
+		case "boot":
+			id, reason := splitUser(rest)
+			q, n = commands.Boot(q, m.User, id, reason)
+		case "oust":
+			id, reason := splitUser(rest)
+			q, n = commands.Oust(q, m.User, id, reason)
+		case "list":
+			n = commands.List(q)
+		case "help":
+			n = commands.Help(name)
+		}
+
+		if n != "" {
+			if !reflect.DeepEqual(oq, q) {
+				logNotification(n)
+				saveChan <- q
+			}
+			notifyChan <- Notification{m.Channel, n}
+		}
+	}
+}
+
+func Save(filename string, saveChan chan queue.Queue) {
+	for q := range saveChan {
+		err := q.Save(filename)
+		if err != nil {
+			log.Printf("Error saving file to %s: %s", filename, err)
+		}
+	}
+}
+
+func Notify(slackConn *slack.Slack, notifyChan chan Notification) {
+	for n := range notifyChan {
+		err := slackConn.PostMessage(n.Channel, n.Message)
+		if err != nil {
+			log.Printf("Error when sending: %s", err)
+		}
+	}
+}
+
+func UpdateUser(userCache *usercache.UserCache, userUpdateChan chan slack.UserInfo) {
+	for u := range userUpdateChan {
+		old_name := userCache.GetUserName(u.Id)
+		userCache.UpdateUserName(u)
+		if old_name == "" {
+			log.Printf("New user %s cached", u.Name)
+		} else {
+			log.Printf("User %s renamed to %s", old_name, u.Name)
+		}
+	}
+}
--- qbot.go
+++ qbot.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"reflect"
 	"strings"
 
 	"github.com/doozr/qbot/command"
@@ -19,50 +18,12 @@ type Notification struct {
 	Message string
 }
 
-func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: qbot <token> <data file>")
-		os.Exit(1)
-	}
+func listen(name string, connection *slack.Slack,
+	messageChan chan slack.RtmMessage, userUpdateChan chan slack.UserInfo) {
 
-	log.Print("Connecting to Slack")
-	slackConn, err := slack.New(os.Args[1])
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println("Getting user list")
-	users, err := slackConn.GetUserList()
-	if err != nil {
-		log.Fatal(err)
-	}
-	userCache := usercache.New(users)
-
-	name := userCache.GetUserName(slackConn.Id)
-	if name == "" {
-		log.Fatal("Could not get username of bot")
-	}
-	log.Printf("Responding to requests directed to @%s", name)
-
-	dumpfile := os.Args[2]
-	log.Printf("Attempting to load queue from %s", dumpfile)
-	q, err := queue.Load(dumpfile)
-
-	notifications := notification.New(userCache)
-	commands := command.New(notifications, userCache)
-
-	messageChan := make(chan slack.RtmMessage, 100)
-	saveChan := make(chan queue.Queue, 5)
-	notifyChan := make(chan Notification, 5)
-
-	go MessageDispatch(name, q, commands, messageChan, saveChan, notifyChan)
-	go Save(dumpfile, saveChan)
-	go Notify(slackConn, notifyChan)
-
-	log.Println("Ready to receive messages")
 	for {
 		// read each incoming message
-		m, err := slackConn.GetEvent()
+		m, err := connection.GetEvent()
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -70,102 +31,90 @@ func main() {
 		// see if we're mentioned
 		if m.Type == "message" {
 			msg := slack.ConvertEventToMessage(m)
-			if strings.HasPrefix(msg.Text, name) || strings.HasPrefix(msg.Text, "<@"+slackConn.Id+">") {
+			if strings.HasPrefix(msg.Text, name) || strings.HasPrefix(msg.Text, "<@"+ connection.Id+">") {
 				messageChan <- msg
 			}
 		}
 
+		// see if it's a user update
 		if m.Type == "user_change" {
 			uc := slack.ConvertEventToUserChange(m)
-			userCache.UpdateUserName(uc.User)
+			userUpdateChan <- uc.User
 		}
 	}
 }
 
-func splitUser(u string) (username string, reason string) {
-	args := strings.SplitN(u, " ", 2)
-	username = args[0]
-	reason = ""
-	if len(args) > 1 {
-		reason = args[1]
+func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: qbot <token> <data file>")
+		os.Exit(1)
+	}
+
+	// Get command line parameters
+	token := os.Args[1]
+	filename := os.Args[2]
+
+	// Instantiate state
+	connection := connectToSlack(token)
+	userCache := getUserList(connection)
+	name := getBotName(userCache, connection.Id)
+	q := loadQueue(filename)
+
+	// Set up command and response processors
+	notifications := notification.New(userCache)
+	commands := command.New(notifications, userCache)
+
+	// Create channels
+	messageChan := make(chan slack.RtmMessage, 100)
+	saveChan := make(chan queue.Queue, 5)
+	notifyChan := make(chan Notification, 5)
+	userUpdateChan := make(chan slack.UserInfo, 5)
+
+	// Start goroutines
+	go MessageDispatch(name, q, commands, messageChan, saveChan, notifyChan)
+	go Save(filename, saveChan)
+	go Notify(connection, notifyChan)
+	go UpdateUser(userCache, userUpdateChan)
+
+	// Dispatch incoming events
+	log.Println("Ready to receive events")
+	listen(name, connection, messageChan, userUpdateChan)
+}
+
+func connectToSlack(token string) (connection *slack.Slack) {
+	log.Print("Connecting to Slack")
+	connection, err := slack.New(token)
+	if err != nil {
+		log.Fatal(err)
 	}
 	return
 }
 
-func MessageDispatch(name string, q queue.Queue, commands command.Command,
-	messageChan chan slack.RtmMessage, saveChan chan queue.Queue, notifyChan chan Notification) {
-
-	logNotification := func(n string) {
-		for _, l := range strings.Split(n, "\n") {
-			if l != "" {
-				log.Println(l)
-			}
-		}
+func getUserList(connection *slack.Slack) (userCache *usercache.UserCache) {
+	log.Println("Getting user list")
+	users, err := connection.GetUserList()
+	if err != nil {
+		log.Fatal(err)
 	}
-
-	for m := range messageChan {
-		parts := strings.SplitN(m.Text, " ", 3)
-
-		if len(parts) < 2 {
-			continue
-		}
-		cmd := parts[1]
-
-		rest := ""
-		if len(parts) > 2 {
-			rest = parts[2]
-		}
-
-		oq := q
-		n := ""
-
-		switch cmd {
-		case "join":
-			q, n = commands.Join(q, m.User, rest)
-		case "leave":
-			q, n = commands.Leave(q, m.User, rest)
-		case "done":
-			q, n = commands.Done(q, m.User)
-		case "yield":
-			q, n = commands.Yield(q, m.User)
-		case "barge":
-			q, n = commands.Barge(q, m.User, rest)
-		case "boot":
-			id, reason := splitUser(rest)
-			q, n = commands.Boot(q, m.User, id, reason)
-		case "oust":
-			id, reason := splitUser(rest)
-			q, n = commands.Oust(q, m.User, id, reason)
-		case "list":
-			n = commands.List(q)
-		case "help":
-			n = commands.Help(name)
-		}
-
-		if n != "" {
-			if !reflect.DeepEqual(oq, q) {
-				logNotification(n)
-				saveChan <- q
-			}
-			notifyChan <- Notification{m.Channel, n}
-		}
-	}
+	userCache = usercache.New(users)
+	return
 }
 
-func Save(filename string, saveChan chan queue.Queue) {
-	for q := range saveChan {
-		err := q.Save(filename)
-		if err != nil {
-			log.Printf("Error saving file to %s: %s", filename, err)
-		}
+func getBotName(userCache *usercache.UserCache, id string) (name string) {
+	name = userCache.GetUserName(id)
+	if name == "" {
+		log.Fatal("Could not get username of bot")
 	}
+	log.Printf("Responding to requests directed to @%s", name)
+	return
 }
 
-func Notify(slackConn *slack.Slack, notifyChan chan Notification) {
-	for n := range notifyChan {
-		err := slackConn.PostMessage(n.Channel, n.Message)
-		if err != nil {
-			log.Printf("Error when sending: %s", err)
-		}
+func loadQueue(filename string) (q queue.Queue) {
+	log.Printf("Attempting to load queue from %s", filename)
+	q, err := queue.Load(filename)
+	if err != nil {
+		log.Fatal("Error loading queue: %s", err)
 	}
+	return
 }
+
