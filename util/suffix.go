8a8c11c35d9c9bd950eab66ef1e5066307993384
Craig Andrews <craig@lazyrobot.org.uk>
2016-12-21 09:16:59 +0000

    Properly handle suffix for 11, 12, 13

--- util/suffix.go
+++ util/suffix.go
@@ -1,7 +1,22 @@
 package util
 
+func unitsDigit(num int) (digit int) {
+	digit = num % 10
+	return
+}
+
+func tensDigit(num int) (digit int) {
+	digit = (num % 100) / 10
+	return
+}
+
 func Suffix(num int) (suffix string) {
-	switch num % 10 {
+	if tensDigit(num) == 1 {
+		suffix = "th"
+		return
+	}
+
+	switch unitsDigit(num) {
 	case 1:
 		suffix = "st"
 	case 2:
